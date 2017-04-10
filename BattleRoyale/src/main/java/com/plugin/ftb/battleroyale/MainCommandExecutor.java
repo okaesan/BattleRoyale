a230134981aaab12cb8d767c4e990babf696ac24
kanaami <boyjar.server@gmail.com>
2017-04-11 00:39:02 +0900

    Add files via upload

---
+++ BattleRoyale/src/main/java/com/plugin/ftb/battleroyale/MainCommandExecutor.java
@@ -0,0 +1,21 @@
+package com.plugin.ftb.battleroyale;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class MainCommandExecutor implements CommandExecutor {
+
+	/*
+	 * onEnable()メソッドで定義したコマンドが実行されるとこのメソッドを通る。
+	 * @param sender コマンド実行者
+	 * @param cmd 実行されたコマンド
+	 * @param label コマンドエイリアス
+	 * @param args コマンドの引数
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
+
+}
---
+++ BattleRoyale/src/main/java/com/plugin/ftb/battleroyale/MainListener.java
@@ -0,0 +1,89 @@
+package com.plugin.ftb.battleroyale;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.WorldBorder;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
+import org.bukkit.entity.Cow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.scoreboard.Scoreboard;
+
+import net.minecraft.server.v1_10_R1.EntityPlayer;
+import net.minecraft.server.v1_10_R1.PacketPlayOutNamedEntitySpawn;
+
+public class MainListener implements Listener {
+	
+	public static BattleRoyale plugin = BattleRoyale.plugin;
+	
+	public static final String TEAM_ALIVE_NAME = BattleRoyale.TEAM_ALIVE_NAME;
+    	public static final String TEAM_DEAD_NAME = BattleRoyale.TEAM_DEAD_NAME;
+    
+    	//キル数カウント
+    	public static HashMap<Player, Integer> killCount = BattleRoyale.killCount;
+	//ポイントカウント
+	public static HashMap<Player, Integer> pointCount = BattleRoyale.pointCount;
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event){
+		String message = event.getMessage();
+		Player player = event.getPlayer();
+		Scoreboard board = plugin.getServer().getScoreboardManager().getMainScoreboard();
+		
+		//DEADチームのみに送信
+		if(board.getTeam(TEAM_DEAD_NAME).hasPlayer(player)){
+			event.setCancelled(true);
+			for(Player p:plugin.getServer().getOnlinePlayers()){
+				if(board.getTeam(TEAM_DEAD_NAME).hasPlayer(p)){
+					p.sendMessage("<" + player.getName() + "> " + message);
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event){
+		Player player = event.getEntity();
+		Player killer = player.getKiller();
+		Scoreboard board = plugin.getServer().getScoreboardManager().getMainScoreboard();
+		
+		//死亡後、DEADチームへ移行
+		if(board.getTeam(TEAM_ALIVE_NAME).hasPlayer(player)){
+			board.getTeam(TEAM_ALIVE_NAME).removePlayer(player);
+			board.getTeam(TEAM_DEAD_NAME).addPlayer(player);
+		}
+		
+		//キル数をカウント
+		if(killCount.containsKey(killer)){
+			killCount.put(killer, killCount.get(killer) + 1);
+		}else{
+			killCount.put(killer, 1);
+		}
+		
+		//最後の1人だった場合ポイントを加算
+		if(board.getTeam(TEAM_ALIVE_NAME).getPlayers().size() == 1){
+			if(pointCount.containsKey(killer)){
+				pointCount.put(killer, pointCount.get(killer) + 1);
+			}else{
+				pointCount.put(killer, 1);
+			}
+		}
+	}
+
+	//デバッグ用
+	public void broadcast(String message){
+		BattleRoyale.broadcast(message);
+	}
+}
